6b1fbe3bdc7a21fbdfb7b95b938a03fb5eb75dd4
ChiaraBecht <c.becht@st.hanze.nl>
2022-05-15 17:28:07 +0200

    changed path to output directory

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -7,12 +7,17 @@ import Bio.Entrez
 """ 
 requirements: for provided PubMed ID download 10 articles which are cited by the provided article
 """
-def output_directory():
+# define E-Mail address
+Bio.Entrez.email = "chiara.becht@web.de"
+
+# set output path
+cwd = Path(__file__).parent.absolute()
+out_dir_p = cwd / 'output'
+
+def output_directory(out_dir_p):
     """
     Create the output directory in the same directory this script is located
     """
-    cwd = Path()
-    out_dir_p = cwd / 'output'
     if not(out_dir_p.exists()):
             out_dir_p.mkdir(parents=True, exist_ok=False)
 
@@ -21,7 +26,7 @@ def download_articles_references(pmid):
     """
     For a given pubmed id download the references from NCBI using Bio.Entrez library
     """
-    Bio.Entrez.email = "chiara.becht@web.de"
+    
     results = Bio.Entrez.read(Bio.Entrez.elink(dbfrom="pubmed",
                                    db="pmc",
                                    LinkName="pubmed_pmc_refs",
@@ -37,7 +42,8 @@ def download_10_referenced_articles(pmid):
     Bio.Entrez.email = "chiara.becht@web.de"
     handle = Bio.Entrez.efetch(db="pmc", id=pmid, rettype="XML", retmode="text",
                            api_key='c4507f85c841d7430a209603112dba418607')
-    with open(f'output/{pmid}.xml', 'wb') as file:
+    #with open(f'output/{pmid}.xml', 'wb') as file:
+    with open(f'{out_dir_p}/{pmid}.xml', 'wb') as file:
         file.write(handle.read())
     
 
@@ -47,17 +53,17 @@ if __name__ == "__main__":
     argparser.add_argument("-n", action="store",
                            dest="n", required=False, type=int,
                            help="Number of references to download concurrently.")
-    argparser.add_argument("-pubmed_id", action="store", type=str, nargs=1, help="Pubmed ID of the article to harvest for references to download.")
+    argparser.add_argument("pubmed_id", action="store", type=str, nargs=1, help="Pubmed ID of the article to harvest for references to download.")
     args = argparser.parse_args()
     print("Getting: ", args.pubmed_id)
     pmid = str(args.pubmed_id)
 
+    # create output directory
+    output_directory(out_dir_p)
+
     # get reference list
     refs = download_articles_references(pmid)
     print(refs)
-
-    # create output directory
-    output_directory()
 
     # set up mulitprocessing task to download 10 referenced articles concurrently
     cpus = mp.cpu_count()
