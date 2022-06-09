8aa2014bd504cac5b0e6bd8d3069e0eabbc01362
Chiara Becht <cbecht@bioinf.nl>
2022-06-09 15:48:32 +0200

    added file to clean up

---
+++ Assignment4/best_kmer.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import shutil
+
+df = pd.read_csv('/students/2021-2022/master/Chiara_DSLS/output/kmers.csv', header = None)
+
+kmers = list(df[0].values)
+n50 = list(df[1].values)
+
+
+index = 0 
+best_index = 0 
+best_n50 = 0
+for i in n50:
+
+    if i > best_n50:
+        best_index = index
+        best_n50 = i
+    index += 1
+    
+best_kmer = kmers[best_index]
+
+
+
+
+
+original = '/students/2021-2022/master/Chiara_DSLS/output/Chiara_{}/contigs.fa'.format(best_kmer)
+target = '/homes/cbecht/programming3/Programming3/Assignment4/output/contig.fa'
+
+shutil.move(original, target)
