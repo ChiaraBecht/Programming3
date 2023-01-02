becaffb0542feaa647c0d88c5ebd5c841cd5a0fb
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-02 15:41:07 +0100

    altered variable name for the file to not overwrtie it with the text io wrapper

--- Assignment6/test/Define_block_size_offset.py
+++ Assignment6/test/Define_block_size_offset.py
@@ -1,7 +1,7 @@
 from io import SEEK_END
 
-def raster_file(file, numb_clients, numb_processes):
-    with open(file) as file:
+def raster_file(filename, numb_clients, numb_processes):
+    with open(filename) as file:
         # get file length measured by characters
         file.seek(0, SEEK_END)
         f_len = file.tell()
@@ -10,7 +10,7 @@ def raster_file(file, numb_clients, numb_processes):
         starts = [0]
         for n in range(1, numb_clients):
             # find ranges based on characters
-            file.seek((f_len // ((numb_clients)/n))) #(n * f_len // 2)
+            file.seek((f_len // ((numb_clients)/n)))
             # find start of next line
             file.readline()
             #store offset
@@ -25,7 +25,7 @@ def raster_file(file, numb_clients, numb_processes):
     # get start and positions per process started for each client
     starts_per_process = []
     stops_per_process = []
-    with open(file) as inf:
+    with open(filename) as inf:
         start_i = 0
         for start, stop in start_stop:
             #print(f'Stops: {stop}')
@@ -47,5 +47,18 @@ def raster_file(file, numb_clients, numb_processes):
             starts_per_process.append(starts_clients)
             stops_per_process.append(stop_clients)
     print(f'start_proc: {starts_per_process}', f'stops proc:{stops_per_process}')
+    return starts, stops, starts_per_process, stops_per_process
 
-raster_file('DUMMY.sam', 3, 2)
+client_starts, client_stops, process_starts, process_stops = raster_file('DUMMY.sam', numb_clients = 3, numb_processes = 3)
+
+def read_data():
+    """
+    Provide client with its start and stop position and as well with the starts and stops for the processes.
+    :param:
+        client_index: number that defines which blocks to choose: don't know yet how to submit that to the client (maybe argparse argument)
+        client_start: line number the client needs to read from
+        client_stop: line number the client need to stop reading
+        process_starts: list of line numbers where processes of the client need to start reading
+        process_stops: list of line numbers where process of the client need to stop reading
+    """
+
