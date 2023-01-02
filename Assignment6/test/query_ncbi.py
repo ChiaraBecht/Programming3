56967b177870397253961f8dc618a0d4895be5a4
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-02 16:54:30 +0100

    download and store genbank file

--- Assignment6/test/query_ncbi.py
+++ Assignment6/test/query_ncbi.py
@@ -1,13 +1,22 @@
 from Bio import Entrez
 from Bio import SeqIO
 
-def query_nbi():
+def query_nbi(gi_id, out_dir):
     """
     """
     Entrez.email = "chiara.becht@web.de"
     handle = Entrez.efetch(db='nucleotide',
-                                id = '253771435', 
-                                rettype = 'gbwithparts', 
+                                id = gi_id, 
+                                rettype = 'gb',#'gbwithparts', 
                                 retmode = 'text', 
                                 api_key='c4507f85c841d7430a209603112dba418607')
+    
     gb_obj = SeqIO.read(handle, 'gb')
+    file_name = out_dir + '/' + gi_id + '.gb'
+    SeqIO.write(gb_obj, file_name, 'gb')
+    
+
+if __name__ == '__main__':
+    gi_id = '253771435'
+    out_dir = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/'
+    query_nbi(gi_id = gi_id, out_dir = out_dir)
