684f1a12b24c8376aec2dbe53cf76f063db069b4
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-03 00:04:14 +0100

    added code to store counts per annotation found for one read in dictionaries

--- Assignment6/test/extract_Annotation_from_gb.py
+++ Assignment6/test/extract_Annotation_from_gb.py
@@ -1,11 +1,19 @@
 from Bio import SeqIO
 import re
+from collections import defaultdict
 
 def extract_GO_EC_numbers(gb_file, read_start, read_stop):
     """
     """
     # read genbank file
     gb_record = SeqIO.read(open(gb_file, 'r'), 'genbank')
+
+    # dictionary for GO numbers
+    GO_num_counts = defaultdict(int)
+    # dictionary for GO descriptions
+    GO_des_counts = defaultdict(int)
+    # dictioanry for EC numbers
+    EC_counts = defaultdict(int)
 
     # check whether read length is one than execute exception, else check which features are spanned
     if (read_stop - read_start) == 1:
@@ -22,18 +30,23 @@ def extract_GO_EC_numbers(gb_file, read_start, read_stop):
             if loc_start <= read_start <= loc_end:
                 print(loc_start, read_start, loc_end)
                 try:
-                    print('GO FUNCTION', locs.qualifiers['GO_function'])
-                    GO = locs.qualifiers['GO_function']
+                    GO = locs.qualifiers['GO_function'][0].split(';')
+                    print('GO FUNCTION', GO)
                     for annot in GO:
-                        GO_anot = annot.split('-')
-                        GO_anot_nr = GO_anot[0]
-                        GO_annot_des = GO_anot[1]
-                        print(GO_anot)
+                        GO_anot = annot.split(' - ')
+                        GO_anot_nr = GO_anot[0].lstrip('')
+                        GO_anot_des = GO_anot[1]
+                        print(GO_anot_nr, GO_anot_des)
+
+                        GO_num_counts[GO_anot_nr] += 1
+                        GO_des_counts[GO_anot_des] += 1
                 except:
                     continue
 
                 try:
-                    print('EC NUMBER', locs.qualifiers['EC_number'])
+                    EC_num = locs.qualifiers['EC_number'][0]
+                    print(EC_num)
+                    EC_counts[EC_num] += 1
                 except:
                     continue
     else:
@@ -49,43 +62,57 @@ def extract_GO_EC_numbers(gb_file, read_start, read_stop):
             if (read_start <= loc_start <= read_stop) & (read_start <= loc_end <= read_stop):
                 print('feature is in this read', read_start, loc_start, loc_end, read_stop)
                 try:
-                    print('GO FUNCTION', locs.qualifiers['GO_function'])
-                    GO = locs.qualifiers['GO_function']
+                    GO = locs.qualifiers['GO_function'][0].split(';')
+                    print('GO FUNCTION', GO)
                     for annot in GO:
-                        GO_anot = annot.split('-')
-                        GO_anot_nr = GO_anot[0]
-                        GO_annot_des = GO_anot[1]
-                        print(GO_anot)
+                        GO_anot = annot.split(' - ')
+                        GO_anot_nr = GO_anot[0].lstrip('')
+                        GO_anot_des = GO_anot[1]
+                        print(GO_anot_nr, GO_anot_des)
+
+                        GO_num_counts[GO_anot_nr] += 1
+                        GO_des_counts[GO_anot_des] += 1
                 except:
                     continue
 
                 try:
-                    print('EC NUMBER', locs.qualifiers['EC_number'])
+                    EC_num = locs.qualifiers['EC_number'][0]
+                    print(EC_num)
+                    EC_counts[EC_num] += 1
                 except:
                     continue
             # get the border cases, where the read starts or ends in a feature (incomplete spanning of a feature by the read)
             if (loc_start <= read_start <= loc_end) or (loc_start <= read_stop <= loc_end):
                 print('read starts in this feature:', loc_start, read_start, loc_end, 'or read ends in this feature:', loc_start, read_stop, loc_end)
                 try:
-                    GO = locs.qualifiers['GO_function']
-                    print('GO FUNCTION', type(GO), GO.split[0].split(';'))
-                    #for annot in GO:
-                    #    GO_anot = annot.split('-')
-                    #    GO_anot_nr = GO_anot[0]
-                    #    GO_annot_des = GO_anot[1]
-                    #    print(GO_anot)
+                    GO = locs.qualifiers['GO_function'][0].split(';')
+                    print('GO FUNCTION', GO)
+                    for annot in GO:
+                        GO_anot = annot.split(' - ')
+                        GO_anot_nr = GO_anot[0].lstrip('')
+                        GO_anot_des = GO_anot[1]
+                        print(GO_anot_nr, GO_anot_des)
+
+                        GO_num_counts[GO_anot_nr] += 1
+                        GO_des_counts[GO_anot_des] += 1
                 except:
                     continue
 
                 try:
-                    print('EC NUMBER', locs.qualifiers['EC_number'])
+                    EC_num = locs.qualifiers['EC_number'][0]
+                    print(EC_num)
+                    EC_counts[EC_num] += 1
                 except:
                     continue
+    
+    print(GO_num_counts)
+    print(GO_des_counts)
+    print(EC_counts)
 
 if __name__ == '__main__':
     file = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/253771435.gb'
-    start = 347
-    stop = 1755
+    start = 1
+    stop = 20000
     dif = stop - start
     print(dif)
     extract_GO_EC_numbers(file, start, stop)
