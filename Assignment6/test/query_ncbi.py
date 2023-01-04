6914e471c362c7664e5376fca6c810b38e3bd861
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-04 01:25:02 +0100

    copied in adjusted version used in the server client model

--- Assignment6/test/query_ncbi.py
+++ Assignment6/test/query_ncbi.py
@@ -1,7 +1,7 @@
 from Bio import Entrez
 from Bio import SeqIO
 
-def query_nbi(gi_id, out_dir):
+def query_ncbi(gi_id):
     """
     """
     Entrez.email = "chiara.becht@web.de"
@@ -12,11 +12,11 @@ def query_nbi(gi_id, out_dir):
                                 api_key='c4507f85c841d7430a209603112dba418607')
     
     gb_obj = SeqIO.read(handle, 'gb')
-    file_name = out_dir + '/' + gi_id + '.gb'
+    file_name = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/' + gi_id + '.gb'
     SeqIO.write(gb_obj, file_name, 'gb')
     
 
 if __name__ == '__main__':
     gi_id = '253771435'
     out_dir = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/'
-    query_nbi(gi_id = gi_id, out_dir = out_dir)
+    query_ncbi(gi_id = gi_id, out_dir = out_dir)
