3c17c54c36d8f0e18728da877503b1dd8a35c462
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-04 17:25:18 +0100

    added flow control using nupmy array

--- Assignment6/test/Server_Client_new.py
+++ Assignment6/test/Server_Client_new.py
@@ -6,10 +6,16 @@ from functools import partial
 from collections import defaultdict
 import os
 import pickle
+import numpy as np 
+import ReadConfig
 from visualise_annotation import produce_visualisation
 from process_line import read_process_line
 from query_ncbi import query_ncbi
 from extract_Annotation_from_gb import extract_GO_EC_numbers
+
+config = ReadConfig.get_config()
+mapping_file = config['mapping_file']
+storage_location = config['storage_location']
 
 def make_server_manager(port, authkey):
     """ Create a manager for the server, listening on the given port.
@@ -42,27 +48,50 @@ def runserver(fn, data):
         print("Gimme something to do here!")
         return
     
-
     print("Sending data!")
     for d in data:
         print('iterate over data and print each item', d)
         shared_job_q.put({'fn' : fn, 'arg' : d})
-        print({'fn' : fn, 'arg' : d})
 
     time.sleep(2)  
 
-    results = []
     # manage results
+    results = np.zeros(len(data), int)
     GO_numbers = defaultdict(int)
     GO_terms = defaultdict(int)
     EC_numbers = defaultdict(int)
-    
+    index = 0
+
     while True:
         try:
             result = shared_result_q.get_nowait()
-            results.append(result)
+            if result['result'][0] != 'DOH':
+                GO_num_dict = result['result'][0]
+                GO_des_dict = result['result'][1]
+                EC_dict = result['result'][2]
+                #results.append(result['result'][3])
+                line_nr = result['result'][3]
+                results[line_nr] += 1
+                index = len(np.where(results == 1)[0])
+
+                # create dictionary that adds counts found for each key
+                for GO_num in GO_num_dict.keys():
+                    GO_numbers[GO_num] += GO_num_dict[GO_num]
+
+                for GO_des in GO_des_dict.keys():
+                    GO_terms[GO_des] += GO_des_dict[GO_des]
+                
+                
+                for EC_num in EC_dict.keys():
+                    EC_numbers[EC_num] += EC_dict[EC_num]
+            else:
+                #results.append(result['result'][1])
+                line_nr = result['result'][1]
+                results[line_nr] += 1
+                index = len(np.where(results == 1)[0])
             print("Got result!", result)
-            if len(results) == len(data):
+            if index == len(data):
+            #if len(results) == len(data):
                 print("Got all results!")
                 break
         except queue.Empty:
@@ -77,42 +106,20 @@ def runserver(fn, data):
     time.sleep(5)
     print("Aaaaaand we're done for the server!")
     manager.shutdown()
-    
-    
-
-    for res in results:
-        if res['result'] != 'DOH':
-            GO_num_dict = res['result'][0]
-            GO_des_dict = res['result'][1]
-            EC_dict = res['result'][2]
-
-            # create dictionary that adds counts found for each key
-            for GO_num in GO_num_dict.keys():
-                GO_numbers[GO_num] += GO_num_dict[GO_num]
-
-            for GO_des in GO_des_dict.keys():
-                GO_terms[GO_des] += GO_des_dict[GO_des]
-            
-            
-            for EC_num in EC_dict.keys():
-                EC_numbers[EC_num] += EC_dict[EC_num]
-            
-    
-    out_dir = '/students/2021-2022/master/Chiara_DSLS/Assignment6/output/'
 
     GO_n_out = 'GO_number_count.pkl'
-    with open(f'{out_dir}/{GO_n_out}', 'wb') as file:
+    with open(f'{storage_location}/{GO_n_out}', 'wb') as file:
         pickle.dump(GO_numbers, file)
     
     GO_t_out = 'GO_terms_count.pkl'
-    with open(f'{out_dir}/{GO_t_out}', 'wb') as file:
+    with open(f'{storage_location}/{GO_t_out}', 'wb') as file:
         pickle.dump(GO_terms, file)
     
     EC_n_out = 'EC_numbers_count.pkl'
-    with open(f'{out_dir}/{EC_n_out}', 'wb') as file:
+    with open(f'{storage_location}/{EC_n_out}', 'wb') as file:
         pickle.dump(EC_numbers, file)    
     
-    produce_visualisation()
+    #produce_visualisation()
 
 
 def make_client_manager(ip, port, authkey):
@@ -166,7 +173,7 @@ def peon(job_q, result_q):
                     result_q.put({'job': job, 'result' : result})
                 except NameError:
                     print("Peon %s found exception on %s!" % (my_name, job['arg']))
-                    result_q.put({'job': job, 'result' : ERROR})
+                    result_q.put({'job': job, 'result' : (ERROR, job['arg'])})
 
         except queue.Empty:
             print("sleepytime for", my_name)
@@ -216,13 +223,13 @@ if __name__ == '__main__':
     AUTHKEY = b'whathasitgotinitspocketsesss?'
 
     lines = []
-    with open('/students/2021-2022/master/Chiara_DSLS/Assignment6/output/alignment.sam') as file:
+    with open(mapping_file) as file:
         # get file length measured by characters
         for i,line in enumerate(file):
             lines.append(i)
     
-    filename = '/students/2021-2022/master/Chiara_DSLS/Assignment6/output/alignment.sam'
-    storage_loc = '/students/2021-2022/master/Chiara_DSLS/Assignment6/output'
+    filename = mapping_file
+    #storage_loc = '/students/2021-2022/master/Chiara_DSLS/Assignment6/output'
     func = partial(instructions, filename)
     
     if args.s:
