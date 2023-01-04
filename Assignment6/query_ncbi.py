f4950930ed5bf8257e2c697df67b489b7e6bd5a5
ChiaraBecht <c.becht@st.hanze.nl>
2023-01-04 23:21:32 +0100

    copied file from test folder

---
+++ Assignment6/query_ncbi.py
@@ -0,0 +1,22 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+def query_ncbi(gi_id):
+    """
+    """
+    Entrez.email = "chiara.becht@web.de"
+    handle = Entrez.efetch(db='nucleotide',
+                                id = gi_id, 
+                                rettype = 'gbwithparts', 
+                                retmode = 'text', 
+                                api_key='c4507f85c841d7430a209603112dba418607')
+    
+    gb_obj = SeqIO.read(handle, 'gb')
+    file_name = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/' + gi_id + '.gb'
+    SeqIO.write(gb_obj, file_name, 'gb')
+    
+
+if __name__ == '__main__':
+    gi_id = '253771435'
+    out_dir = '/students/2021-2022/master/Chiara_DSLS/Assignment6/genbank_cache/'
+    query_ncbi(gi_id = gi_id, out_dir = out_dir)
